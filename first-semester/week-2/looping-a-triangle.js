3003959e1abacde1038431bbd92a55df5874ee7b
CNwante <Myphilothea@gmail.com>
2023-09-25 12:42:50 +0100

    Add solution for looping-a-triangle exercise

---
+++ first-semester/week-2/looping-a-triangle.js
@@ -0,0 +1,10 @@
+// Looping a Triangle Exercise
+
+for (let x = 1; x < 8; x++) {
+
+    let triangle = '';
+    for (let y = 0; y < x; y++) {
+        triangle += "#";
+    }
+    console.log(triangle);
+}
